8cd96df65a6c2864582d2fc83598d7e5a7fa27cf
ilhamhlmi <ilhamhilmi206@gmail.com>
2026-01-16 13:19:53 +0700

    railway db upd

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "centerbeam.proxy.rlwy.net",
+  host: "shuttle.proxy.rlwy.net",
   user: "root",
-  password: "iJsBtnAnroQzPlkeRUmUvdrGwpLAIEJd",
+  password: "eVveqDLHwYHRwLRGJeVLITTqMjOEbqCp",
   database: "railway",
-  port: 30911,
+  port: 27470,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
